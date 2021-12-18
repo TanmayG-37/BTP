c21d3d5946c87260c378294d59579b6542949e0f
TanmayG-37 <56036790+TanmayG-37@users.noreply.github.com>
2021-12-18 22:32:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,6 +74,112 @@ class IF:
 
 
 
+from scipy.special import erfc
+
+class Chauv:
+    def __init__(self,n_estimators,strictness):
+        self.L=n_estimators
+        self.strictness = strictness
+        self.mean=[]
+        self.std=[]
+        self.weights=[]
+        self.size=0
+        self.scores=[]
+        self.subset = []
+
+
+    def get_bins(self):
+        # defining the weights for each feature
+        for i in range(self.L):
+            self.weights.append(np.array([random.random() for j in range(self.size)])) 
+            
+
+    def clusters(self,data):
+        self.subset = random.sample(list(data.columns),int(self.size*self.strictness))
+        return
+
+
+    def fit(self,data):
+        self.size=len(data[0])
+        print(self.size)
+        # get_bins gives us the factors for each feature
+        self.get_bins()
+        # clusters functions hashes the datapoints into groups so that we can detect local outliers
+        self.clusters(pd.DataFrame(data))
+        self.mean=data.mean(axis=0)
+        self.std=data.std(axis=0)+(10**-5) # to avoid division by 0 error
+        return None
+
+
+    def chauvenet(self,data):
+        N = len(data)                
+        criterion = 1.0/(2*N)         # Chauvenet's criterion
+        d = abs(data-self.mean)/self.std         
+        d /= 2.0**0.5                
+        prob = erfc(d)               # Area normal dist.    
+        print(prob)
+        filter = prob >= criterion 
+        # print(filter)  
+        self.scores = filter
+        return filter                
+
+
+
+
+class Robust:
+	def __init__(self,n_estimators,strictness):
+		self.L=n_estimators
+		self.strictness = strictness
+		self.med=[]
+		self.std=[]
+		self.weights=[]
+		self.size=0
+		self.scores=[]
+		self.subset = []
+
+
+	def get_bins(self):
+		# defining the weights for each feature
+		for i in range(self.L):
+			self.weights.append(np.array([random.random() for j in range(self.size)])) 
+			
+
+	def clusters(self,data):
+		self.subset = random.sample(list(data.columns),int(self.size*self.strictness))
+		return
+
+
+	def fit(self,data):
+		self.size=len(data[0])
+		print(self.size)
+		# get_bins gives us the factors for each feature
+		self.get_bins()
+		# clusters functions hashes the datapoints into groups so that we can detect local outliers
+		self.clusters(pd.DataFrame(data))
+		self.med = pd.DataFrame(data).median(axis=0)
+		med = np.median(data)
+		x   = abs(data-med)
+		MAD = np.median(x)
+		self.mad = MAD
+		return None
+
+
+
+	def score(self,data):
+		# improve calculation of score using numpy arrays for matrix multiplication
+		# # 1. scoring on the based of standard deviation (regressive annd a lot of calculations)
+		temp_data=(0.6745*(pd.DataFrame(data)-self.med))/self.mad
+		for i in range(self.L):
+			self.scores.append((np.dot(temp_data,self.weights[i].transpose()))/self.size) 
+		return self.scores
+
+
+
+
+
+
+
+
 
 
 
@@ -105,10 +211,11 @@ def take_input():
 
 
 
-
+rno = ''
 @app.route('/data', methods=['GET', 'POST'])
 def data():
 	global f
+	global rno
 	import time
 	start_time = time.time()
 
@@ -123,8 +230,8 @@ def data():
 
 	# Reading the data
 	data = pd.read_csv('static/'+str(f.filename))
-	data = data.astype('float')
-	data = data.dropna(axis='columns')
+	# data = data.astype('float')
+	# data = data.dropna(axis='columns')
 
 
 
@@ -150,7 +257,7 @@ def data():
 
 
 	if algo == 'RZScore':
-		obj=RZScore(5)
+		obj=RZScore(50)
 		obj.fit(data)
 		obj.score(data)
 		temp_scores = obj.scores
@@ -166,9 +273,9 @@ def data():
 		t1 = q3+(1.5)*iqr; t2 = q1-(1.5)*iqr
 		for i in range(len(new)):
 			if new[i]>t1 or new[i]<t2:
-				out1.append(1)
+				out.append(1)
 			else:
-				out1.append(0)
+				out.append(0)
 
 
 		# extreme = []
@@ -191,7 +298,7 @@ def data():
 
 
 	# 3rd method HBOS
-	else:
+	elif algo=='HBOS':
 		from pyod.models import hbos
 		model = hbos.HBOS()
 		model.fit(data)
@@ -199,9 +306,62 @@ def data():
 		temp_scores = model.decision_function(data)
 
 
+	#4th method Robust
+	if algo=='Robust RZ'
+		obj=Robust(5,0.1)
+		obj.fit(data)
+		obj.score(data)
+		temp_scores = obj.scores
+		new = np.array([0.0 for i in range(len(obj.scores[0]))])
+		for i in range(len(temp_scores)):
+			new+=temp_scores[i]
+
+		
+		q1 = np.quantile(new,.25)
+		q3 = np.quantile(new,.75)
+		iqr = q3-q1
+		out = []
+		t1 = q3+(1.5)*iqr; t2 = q1-(1.5)*iqr
+		for i in range(len(new)):
+			if new[i]>t1 or new[i]<t2:
+				out.append(1)
+			else:
+				out.append(0)
+
+
+	# 5th method
+	else:
+		obj=Chauv(5,0.1)
+		obj.fit(data)
+		obj.chauvenet(data)
+		# print(obj.scores)
+		new = []
+		for i in obj.scores:
+		    new.append(sum(i))
+		# new
+		new = new
+		# for i in range(len(new)):
+		#     print(new[i])
+		q1 = np.quantile(new,.25)
+		q3 = np.quantile(new,.75)
+		iqr = q3-q1
+		out11 = []
+		for i in range(len(new)):
+		    if new[i]>(q3+(1.5)*iqr) or new[i]<(q1-(1.5)*iqr):
+		        out11.append(1)
+		    else:
+		        out11.append(0)
+
+
+
+
+
+
+
+
 	# We declare a point as an outlier if it occurs in 2 or more methods
 	output = 0
-	labels = out
+	# labels = out
 	for i in range(len(out)):
 		if out[i]==1:
 			output+=1
@@ -236,6 +396,14 @@ def data():
 
 
 
+	# saving outliers indices
+	temp_dict = {'Outlier':out}
+	df = pd.DataFrame(temp_dict)
+	rno = str(random.randint(0, 1000000))
+	df.to_csv('static/outlier'+rno+'.csv')
+
+
+
 	# score analysis
 	temp_scores = temp_scores + min(temp_scores)
 	temp_scores = temp_scores / max(temp_scores)
@@ -243,6 +411,14 @@ def data():
 	mean = round(np.mean(temp_scores),3)
 
 	return render_template('base2.html', outliers=output, number=number, features=features, impurity=impurity, std=std, mean=mean)
+
+
+
+@app.route('/download', methods=['GET', 'POST'])
+def download():
+	global rno
+	print('ahahahahaha')
+	return send_file('static/outlier'+rno+'.csv', as_attachment=True, attachment_filename='out.csv')
 
 
 
